b01dd9944b01b330d2f62f1246ca88917faf5b70
kyle.martinez <kmartinez@convio.com>
2013-04-01 15:11:00 -0500

    first test for watirmark koans

---
+++ controllers/donation_form_controller.rb
@@ -0,0 +1,26 @@
+class DonationFormView < Watirmark::Page
+
+  def home(model)
+    Page.browser.goto( "file:///" + File.dirname(__FILE__) + "/html/donation_form.html")
+  end
+
+  def create(model)
+    home(model)
+  end
+
+  def close_browser
+    Page.browser.close
+  end
+end
+
+class DonationFormController < Watirmark::WebPage::Controller
+  @view = DonationFormView
+
+  def after_each
+    @view.close_browser
+  end
+
+  def submit
+    @view.submit.click
+  end
+end
---
+++ koans/about_keywords.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+# -*- ruby -*-
+
+require File.expand_path(File.dirname(__FILE__) + '/edgecase')
+
+
+class AboutKeywordsView < DonationFormView
+  keyword(:amount) { browser.text_field(:id, "amount") }
+  keyword(:firstname) { browser.text_field(:id, "first_name") }
+  keyword(:lastname) { browser.text_field(:id, "last_name") }
+  keyword(:card_type,
+          ["Visa"] => "A",
+          ["Discover"] => "B"
+
+  ) { browser.radio(:name, "radgroup") }
+  keyword(:cardnumber) { browser.text_field(:id, "credit_card") }
+  keyword(:cvv) { browser.text_field(:id, "credit_card_cvv") }
+  keyword(:exp_date) { browser.text_field(:id, "exp_date") }
+  keyword(:submit)   { browser.button(:value, "Submit")}
+end
+
+class AboutKeywordsController < DonationFormController
+  @view = AboutKeywordsView
+end
+
+class AboutKeywords < EdgeCase::Koan
+
+  def test_missing_keyword
+    keywords = {:amount => "10.00",
+                :firstname => "FisrtName",
+                :lastname => "LastName",
+                :card_type => "Visa",
+                :cardnumber => "4111111111111111",
+                :cvv => "111"}
+
+    AboutKeywordsController.new(keywords).run :create
+  end
+end
