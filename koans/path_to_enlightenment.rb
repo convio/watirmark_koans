99f01732c6a20ff466208973806e322c1c427710
kyle.martinez <kmartinez@convio.com>
2013-04-02 15:03:15 -0500

    adding tests to the path to enlightenment

--- koans/path_to_enlightenment.rb
+++ koans/path_to_enlightenment.rb
@@ -1,3 +1,5 @@
 # The path to Ruby Enlightenment starts with the following:
 $LOAD_PATH << File.dirname(__FILE__)
 require "about_keywords"
+require "about_populate_data"
+require "about_verify_keywords"
