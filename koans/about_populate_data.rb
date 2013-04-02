827dd87579a768b32603ed956c74bf655a2c4a1b
kyle.martinez <kmartinez@convio.com>
2013-04-02 08:28:26 -0500

    test_populate_cardnumber

--- koans/about_populate_data.rb
+++ koans/about_populate_data.rb
@@ -9,9 +9,9 @@ class AboutPopulateView < DonationFormView
   keyword(:firstname) { browser.text_field(:id, "first_name") }
   keyword(:lastname) { browser.text_field(:id, "last_name") }
   keyword(:gift_type) { browser.select_list(:id, "giftType") }
-  keyword(:street)   { browser.text_field(:id, "street_name")}
-  keyword(:city)   { browser.text_field(:id, "city")}
-  keyword(:state)   { browser.select_list(:id, "state")}
+  keyword(:street) { browser.text_field(:id, "street_name") }
+  keyword(:city) { browser.text_field(:id, "city") }
+  keyword(:state) { browser.select_list(:id, "state") }
   keyword(:card_type,
           ["Visa"] => "A",
           ["Discover"] => "B",
@@ -20,42 +20,134 @@ class AboutPopulateView < DonationFormView
   keyword(:cardnumber) { browser.text_field(:id, "credit_card") }
   keyword(:cvv) { browser.text_field(:id, "credit_card_cvv") }
   keyword(:exp_date) { browser.text_field(:id, "exp_date") }
-  keyword(:submit)   { browser.button(:value, "Submit")}
+  keyword(:submit) { browser.button(:value, "Submit") }
 end
 
-class AboutPopulateController < DonationFormController
+class PopulateDataOne < DonationFormController
   @view = AboutPopulateView
+
+  def populate_data
+    @view.amount = "10.00"
+    @view.firstname = "FirstName"
+    @view.lastname = "LastName"
+    @view.cardnumber = "4111111111111111"
+    @view.cvv = "111"
+    @view.exp_date = #Enter value to populate field
+    submit
+  end
+end
+
+class PopulateDataTwo < DonationFormController
+  @view = AboutPopulateView
+
+  def populate_data
+    @view.amount = @model.amount
+    @view.firstname = @model.firstname
+    @view.lastname = @model.lastname
+    @view.cardnumber = @model.cardnumber
+    @view.cvv = @model.cvv
+    @view.exp_date = #enter model value to populate field
+    submit
+  end
+
+end
+
+class PopulateDataThree < DonationFormController
+  @view = AboutPopulateView
+
+  def populate_data
+    super
+  end
+end
+
+class PopulateDataFour < DonationFormController
+  @view = AboutPopulateView
+
+  def populate_data
+    super           #calls populate_amount, and populate_exp_date
+  end
 
   def populate_amount
     @view.amount = @model.amount
   end
 
-  def populate_firstname
-    @view.firstname = @model.firstname
-  end
-
-  def populate_lastname
-    @view.lastname = @model.lastname
-  end
-
-  def populate_cardnumber
+  def populate_exp_date
 
   end
-
 end
+
+class PopulateDataFive < DonationFormController
+  @view = AboutPopulateView
+
+  def populate_data
+    populate_required_info
+  end
+
+  def populate_required_info
+    @view.amount = @model.amount
+    @view.firstname = @model.firstname
+    @view.lastname = @model.lastname
+    @view.cardnumber = @model.cardnumber
+    @view.cvv = @model.cvv
+    @view.exp_date = #enter model value to populate field
+    submit
+  end
+end
+
+
+
 
 class AboutPopulateData < EdgeCase::Koan
 
-  def test_populate_cardnumber
-    keywords = {:amount => "10.00",
-                :firstname => "FisrtName",
-                :lastname => "LastName",
-                :card_type => "American Express",
-                :cardnumber => "4111111111111111",
-                :cvv => "111",
-                :exp_date => "05/09/2014"}
-
-    AboutPopulateController.new(keywords).run :create
+  def test_populate_data1
+    PopulateDataOne.new().run :create
   end
 
+  def test_populate_data2
+    model = {:amount => "10.00",
+             :firstname => "FisrtName",
+             :lastname => "LastName",
+             :card_type => "American Express",
+             :cardnumber => "4111111111111111",
+             :cvv => "111",
+             :exp_date => "05/09/2014"}
+
+    PopulateDataTwo.new(model).run :create
+  end
+
+  def test_populate_data3
+    model = {:amount => "10.00",
+             :firstname => "FisrtName",
+             :lastname => "LastName",
+             :card_type => "American Express",
+             :cardnumber => "4111111111111111",
+             :cvv => "111",
+             :exp_date => nil} #missing exp_date value in model
+
+    PopulateDataThree.new(model).run :create
+  end
+
+  def test_populate_data4
+    model = {:amount => "10.00",
+             :firstname => "FisrtName",
+             :lastname => "LastName",
+             :card_type => "American Express",
+             :cardnumber => "4111111111111111",
+             :cvv => "111",
+             :exp_date => "05/09/2014"}
+
+    PopulateDataFour.new(model).run :create
+  end
+
+  def test_populate_data5
+    model = {:amount => "10.00",
+             :firstname => "FisrtName",
+             :lastname => "LastName",
+             :card_type => "American Express",
+             :cardnumber => "4111111111111111",
+             :cvv => "111",
+             :exp_date => "05/09/2014"}
+
+    PopulateDataFive.new(model).run :create
+  end
 end
