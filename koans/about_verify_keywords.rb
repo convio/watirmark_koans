2fcbfed383b580f51ef61c11049b80162615b865
kyle.martinez <kmartinez@convio.com>
2013-04-02 13:22:33 -0500

    test verify keyword 2

--- koans/about_verify_keywords.rb
+++ koans/about_verify_keywords.rb
@@ -10,7 +10,8 @@ class AboutVerifyKeywordView < ContactInfoView
   verify_keyword(:lastname) { contact_table.row(:text, /Last Name/).cell(:index, 1) }
   verify_keyword(:street) { contact_table.row(:text, /Street Address/).cell(:index, 1) }
   verify_keyword(:city) { contact_table.row(:text, /City/).cell(:index, 1) }
-  verify_keyword(:zip) { contact_table.row(:text, /Postal/).cell(:index, 1) }
+  verify_keyword(:state) { contact_table.row(:text, /State/).cell(:index, 1) }
+  verify_keyword(:zip) { contact_table.row(:text, /Postal Codee/).cell(:index, 1) }
 
   #transaction information
   verify_keyword(:transactiondate) { |name| locate_row(name).cell(:index, 1) }
@@ -23,7 +24,7 @@ class AboutVerifyKeywordView < ContactInfoView
   end
 end
 
-class VerifyKeyword < ContactInfoController
+class VerifyKeywords < ContactInfoController
   @view = AboutVerifyKeywordView
 end
 
@@ -32,7 +33,18 @@ class AboutVerifyKeyword < EdgeCase::Koan
 
   def test_verify_keyword1
     verify_keywords = {:firstname => "Jonny"}
-    VerifyKeyword.new(verify_keywords).run :verify
+    VerifyKeywords.new(verify_keywords).run :verify
+  end
+
+  def test_verify_keyword2
+    verify_keywords = {:firstname => "Bobby",
+                       :lastname => "Joe",
+                       :street => "1235 Oak Hill Lane",
+                       :city => "Austin",
+                       :state => "Texas",
+                       :zip => "78754"}
+
+    VerifyKeywords.new(verify_keywords).run :verify  #Cannot locate element on page. Maybe something is wrong the the controllers view
   end
 
 end
