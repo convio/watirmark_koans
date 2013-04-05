6ccfa04952febece2b67ad07af27aba9d427d94a
kyle.martinez <kmartinez@convio.com>
2013-04-05 11:39:05 -0500

    remove keywords that are not being used.

--- koans/about_models.rb
+++ koans/about_models.rb
@@ -8,15 +8,6 @@ class AboutModelsView < DonationFormView
   keyword(:amount) { browser.text_field(:id, "amount") }
   keyword(:firstname) { browser.text_field(:id, "first_name") }
   keyword(:lastname) { browser.text_field(:id, "last_name") }
-  keyword(:gift_type) { browser.select_list(:id, "giftType") }
-  keyword(:street) { browser.text_field(:id, "street_name") }
-  keyword(:city) { browser.text_field(:id, "city") }
-  keyword(:state) { browser.select_list(:id, "state") }
-  keyword(:card_type,
-          ["Visa"] => "A",
-          ["Discover"] => "B",
-          ["American Express"] => "C"
-  ) { browser.radio(:name, "radgroup") }
   keyword(:cardnumber) { browser.text_field(:id, "credit_card") }
   keyword(:cvv) { browser.text_field(:id, "credit_card_cvv") }
   keyword(:exp_date) { browser.text_field(:id, "exp_date") }
