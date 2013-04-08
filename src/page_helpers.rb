90475e43807a9e5814ab419ccb868aeba81aacd4
hmcgowan <hugh_mcgowan@yahoo.com>
2013-04-08 15:41:07 -0500

    clean up and refactor

--- src/page_helpers.rb
+++ src/page_helpers.rb
@@ -1,15 +1,24 @@
 module PageHelpers
   def show_page name
-    url = "file:///" + File.dirname(__FILE__) + "/../html/#{name}"
-    browser.execute_script "document.getElementById('page').src = \"#{url}\""
+    goto uri_path(name)
   end
 
   def browser
     @@browser ||= begin
       Watirmark::Session.instance.openbrowser
-      Watirmark::Page.browser.goto "file:///" + File.dirname(__FILE__) + "/../html/koans.html"
+      Watirmark::Page.browser.goto uri_path('koans.html')
       Watirmark::Page.browser
     end
+  end
+
+  private
+
+  def goto(path)
+    browser.execute_script "document.getElementById('page').src = \"#{path}\""
+  end
+
+  def uri_path(name)
+    "file://" + File.dirname(__FILE__) + "/../html/#{name}"
   end
 
 end
