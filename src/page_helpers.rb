b324b74142d14c317678c99432bd1b1af8656f0c
kyle.martinez <kmartinez@convio.com>
2013-04-09 08:43:15 -0500

    execute_script throwing error if iframe src is already set to the path

--- src/page_helpers.rb
+++ src/page_helpers.rb
@@ -17,7 +17,7 @@ module PageHelpers
 
   def goto(path)
     browser
-    @@browser.execute_script "document.getElementById('page').src = \"#{path}\""
+    @@browser.execute_script "document.getElementById('page').src = \"#{path}\"" unless @@browser.iframe(:id, "page").src == path
   end
 
   def file_path(name)
