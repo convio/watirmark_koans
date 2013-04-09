a1a3f27b3fbfab5e24a5427aacb0f7d747d13b64
kyle.martinez <kmartinez@convio.com>
2013-04-09 14:28:26 -0500

    updating about traits to use simpler form

--- src/about_traits.rb
+++ src/about_traits.rb
@@ -7,44 +7,22 @@ module Watirmark::Model
     lastname  {"Smith"}
   end
 
-  trait :address do
-    street {"123 Oak Hill Lane"}
-    city {"Austin"}
-    state {"Texas"}
-  end
-
-  trait :credit_card do
-    cardnumber {"4111111111111111"}
-    cvv {"111"}
-    exp_date {"05/09/2014"}
-  end
-
   trait :donor do
-    traits :person, :address, :credit_card
+    traits :person
     amount {"10.00"}
-    gift_type {"Personal"}
   end
-
 end
 
 class AboutTraits < EdgeCase::Koan
 
   def setup
-    show_page "donation_form.html"
+    show_page "about_traits.html"
   end
 
   class DonationFormPage < Watirmark::Page
     keyword(:amount) { browser.text_field(:id, "amount") }
     keyword(:firstname) { browser.text_field(:id, "first_name") }
     keyword(:lastname) { browser.text_field(:id, "last_name") }
-    keyword(:gift_type) { browser.select_list(:id, "giftType") }
-    keyword(:street) { browser.text_field(:id, "street_name") }
-    keyword(:city) { browser.text_field(:id, "city") }
-    keyword(:state) { browser.select_list(:id, "state") }
-    keyword(:cardnumber) { browser.text_field(:id, "credit_card") }
-    keyword(:cvv) { browser.text_field(:id, "credit_card_cvv") }
-    keyword(:exp_date) { browser.text_field(:id, "exp_date") }
-    keyword(:submit) { browser.button(:value, "Submit") }
   end
 
   class Donationform < Watirmark::WebPage::Controller
@@ -57,13 +35,6 @@ class AboutTraits < EdgeCase::Koan
       amount { "10.00"}
       firstname { "Robby" }
       lastname { "Smith" }
-      gift_type { "Personal" }
-      street { "123 Oak Hill Lane" }
-      city { "Austin" }
-      state { "Texas" }
-      cardnumber { "4111111111111111" }
-      cvv { "111" }
-      exp_date { "05/09/2014" }
     end
   end
 
@@ -73,7 +44,7 @@ class AboutTraits < EdgeCase::Koan
   end
 
   def test_traits_in_trait
-    assert_equal __([:person, :address, :credit_card]), Watirmark::Model::Traits.instance[:donor].traits
+    assert_equal __([:person]), Watirmark::Model::Traits.instance[:donor].traits
   end
 
   def test_model_traits
@@ -85,13 +56,6 @@ class AboutTraits < EdgeCase::Koan
     assert_equal __("10.00"), model.amount
     assert_equal __("Robby"), model.firstname
     assert_equal __("Smith"), model.lastname
-    assert_equal __("Personal"), model.gift_type
-    assert_equal __("123 Oak Hill Lane"), model.street
-    assert_equal __("Austin"), model.city
-    assert_equal __("Texas"), model.state
-    assert_equal __("4111111111111111"), model.cardnumber
-    assert_equal __("111"), model.cvv
-    assert_equal __("05/09/2014"), model.exp_date
   end
 
   def test_model_with_taits
@@ -99,13 +63,6 @@ class AboutTraits < EdgeCase::Koan
     assert_equal __("10.00"), model.amount
     assert_equal __("Robby"), model.firstname
     assert_equal __("Smith"), model.lastname
-    assert_equal __("Personal"), model.gift_type
-    assert_equal __("123 Oak Hill Lane"), model.street
-    assert_equal __("Austin"), model.city
-    assert_equal __("Texas"), model.state
-    assert_equal __("4111111111111111"), model.cardnumber
-    assert_equal __("111"), model.cvv
-    assert_equal __("05/09/2014"), model.exp_date
   end
 
   def test_model_without_traits_in_controller
@@ -117,13 +74,6 @@ class AboutTraits < EdgeCase::Koan
     assert_equal __("10.00"), page.amount.value
     assert_equal __("Robby"), page.firstname.value
     assert_equal __("Smith"), page.lastname.value
-    assert_equal __("Personal"), page.gift_type.value
-    assert_equal __("123 Oak Hill Lane"), page.street.value
-    assert_equal __("Austin"), page.city.value
-    assert_equal __("Texas"), page.state.value
-    assert_equal __("4111111111111111"), page.cardnumber.value
-    assert_equal __("111"), page.cvv.value
-    assert_equal __("05/09/2014"), page.exp_date.value
   end
 
   def test_model_with_traits_in_controller
@@ -135,12 +85,5 @@ class AboutTraits < EdgeCase::Koan
     assert_equal __("10.00"), page.amount.value
     assert_equal __("Robby"), page.firstname.value
     assert_equal __("Smith"), page.lastname.value
-    assert_equal __("Personal"), page.gift_type.value
-    assert_equal __("123 Oak Hill Lane"), page.street.value
-    assert_equal __("Austin"), page.city.value
-    assert_equal __("Texas"), page.state.value
-    assert_equal __("4111111111111111"), page.cardnumber.value
-    assert_equal __("111"), page.cvv.value
-    assert_equal __("05/09/2014"), page.exp_date.value
   end
 end
