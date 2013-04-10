f41f311fe3b56c1cea4871b11068ada552e472d1
hmcgowan <hugh_mcgowan@yahoo.com>
2013-04-10 10:33:56 -0500

    remove unnecessary instance variable

--- templates/resources/example_template.rb
+++ templates/resources/example_template.rb
@@ -6,15 +6,14 @@ require_relative 'footer_template'
 module Template
   class Example
     def initialize &block
-      @html = ""
       @content = block
     end
 
     def generate
-      @html = Header.new(%Q{  <link href="example.css" rel="stylesheet" type="text/css">}).generate
-      @html += Form.new(&@content).generate
-      @html += Doc.new.generate
-      @html += Footer.new.generate
+      html = Header.new(%Q{  <link href="example.css" rel="stylesheet" type="text/css">}).generate
+      html += Form.new(&@content).generate
+      html += Doc.new.generate
+      html += Footer.new.generate
     end
   end
 end
