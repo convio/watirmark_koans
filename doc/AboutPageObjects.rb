7e431affa885d4551533911d5ae50e06c5938952
hmcgowan <hugh_mcgowan@yahoo.com>
2013-04-10 14:56:59 -0500

    minor doc changes

--- doc/AboutPageObjects.rb
+++ doc/AboutPageObjects.rb
@@ -4,14 +4,39 @@ module AboutPageObjectsDocHelper
   include PageHelpers
   extend self
 
+  def page_object_view
+    %Q{<pre class="ruby">
+
+class ContactPage < Watirmark::Page
+  keyword(:first_name) { browser.text_field(:id => "first_name") }
+  keyword(:last_name) { browser.text_field(:id => "last_name") }
+end
+      </pre>}
+  end
+
+  def test_about_page_objects
+    document "Page Objects" do
+      %Q{
+         <p>Page Objects describe a design pattern that provides an interface
+            between a UI interface and a class.
+         <p>We use it because it allows us to refer to any UI element with a keyword and
+            use that keyword in the rest of our test infrastructure. If the UI changes,
+            we can update the Page Object and not have to change any of the framework or test cases.
+      }
+    end
+  end
+
   def test_use_page_objects_to_read_values_doc
-    document do
-      "This is documentation for test_use_page_objects_to_read_values"
+    document "Page Objects", "Reading Values" do
+      %Q{
+         To get a value from a page object, simply
+         #{page_object_view}
+      }
     end
   end
 
   def test_use_page_objects_to_write_values_doc
-    document do
+    document "Page Objects" do
       "This is documentation for test_use_page_objects_to_write_values"
     end
   end
--- src/about_page_objects.rb
+++ src/about_page_objects.rb
@@ -5,22 +5,19 @@ class AboutPageObjects < EdgeCase::Koan
     show_page "page_object.html"
   end
 
-  class DonationPage < Watirmark::Page
+  class ContactPage < Watirmark::Page
     keyword(:first_name) { browser.text_field(:id => 'first_name') }
     keyword(:last_name) { browser.text_field(:id => 'last_name') }
-    keyword(:gift_type) { browser.select_list(:id => 'gift_type') }
-    keyword(:remember_me) { browser.checkbox(:id => 'remember_me') }
-    keyword(:submit) { browser.button(:id => 'gift_type') }
   end
 
   def test_use_page_objects_to_read_values
-    page = DonationPage.new
+    page = ContactPage.new
     page.first_name.value = "Suzie"
     assert_equal __('Suzie'), page.first_name.value
   end
 
   def test_use_page_objects_to_write_values
-    page = DonationPage.new
+    page = ContactPage.new
     assert_equal __('Once'), page.gift_type.value
     page.gift_type = 'Recurring'
     assert_equal __('Recurring'), page.gift_type.value
