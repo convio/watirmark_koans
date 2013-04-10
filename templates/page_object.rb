f17096eb0d69168e4eacfa4aa3f4f04775bd2eba
hmcgowan <hugh_mcgowan@yahoo.com>
2013-04-10 14:56:02 -0500

    simplify template

--- templates/page_object.rb
+++ templates/page_object.rb
@@ -11,19 +11,6 @@ template = Template::Example.new do
     <td align="right">Last Name:</td>
     <td><input type="text" id="last_name" value="Smith"></td>
   </tr>
-  <tr>
-    <td align="right">Gift Type:</td>
-    <td>
-      <select id="gift_type">
-      <option value="Once">Once</option>
-      <option value="Recurring">Recurring</option>
-      </select>
-    </td>
-  </tr>
-  <tr>
-    <td align="right">Remember me:</td>
-    <td><input type="checkbox" id="remember_me"></td>
-  </tr>
 </table>
 }
 end
