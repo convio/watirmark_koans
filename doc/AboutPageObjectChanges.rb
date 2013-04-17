2e8f6c8434c826d2081f2661a98178104f51821c
hmcgowan <hugh_mcgowan@yahoo.com>
2013-04-17 11:44:55 -0500

    update docs

--- doc/AboutPageObjectChanges.rb
+++ doc/AboutPageObjectChanges.rb
@@ -28,14 +28,12 @@ end
     <p>In this test, we've just gotten a new release and a test that used to pass, but now fails.
        We know that the developer has made some changes to the UI so let's look there.
 
-    <p>You should be able to inspect the HTML for any element by right clicking the element*.
+    <p>You should be able to inspect the HTML for any element by right clicking the element.
 
     <p>Once you see the HTML you can compare it with the Watir call in the Page Object and
        update it accordingly
 
     <p>Look at the code for this test case and update it to work with the new UI
-
-    <p valign=bottom>*<i>Internet Explorer will require you to install the Developer Tools<i/>
     }
   end
 
@@ -43,8 +41,9 @@ end
     %Q{
     #{updated_page_object_view}
 
-    <p>In this release, the developer added a new field. Update the Page Object
-       with a new keyword to handle populating the middle name.
+    <p>In this release, the developer added a new field for a middle name.
+
+    <p>Update the page object with a new keyword to handle populating the middle name.
     }
   end
 end
