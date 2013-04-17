a7facc813a8cd622c123785965c9203243fa21f8
hmcgowan <hugh_mcgowan@yahoo.com>
2013-04-17 13:56:26 -0500

    work on cleaning up models

--- doc/AboutModels.rb
+++ doc/AboutModels.rb
@@ -4,110 +4,161 @@ module AboutModelsDoc
   include DocHelpers
   extend self
 
-  def donor_model
+  def model
     ruby %Q{
-class SingleDonorModel < Watirmark::Model.factory
-  keywords SingleDonorModelView.keywords
-  traits :donor_address
-  model_type Donor
+class ContactPage < Watirmark::Page
+  keyword(:first_name) { browser.text_field(:id => "first.name") }
+  keyword(:middle_name) { browser.text_field(:id => "first.name") }
+  keyword(:last_name)  { browser.text_field(:id => "last.name")  }
+end
+
+class ContactModel < Watirmark::Model.factory
+  keywords ContactPage.keywords
+end
+}
+  end
+
+  def using_simple_model
+    ruby %Q{
+model = ContactModel.new
+model.first_name = "John"
+assert_equal "John", model.first_name
+assert_equal nil, model.last_name
+}
+  end
+
+  def model_with_defaults
+    ruby %Q{
+class ContactModel < Watirmark::Model.factory
+  keywords ContactPage.keywords
   defaults do
-      first_name {"John"}
-      last_name  {"Smith"}
+    first_name {"first_\#{uuid}"}
+    last_name  {"last_\#{uuid}"}
+  end
+end
+}
   end
 
-  def full_name
-    "\#{last_name}, \#{first_name}"
-  end
-end}
+  def using_model_with_defaults
+    ruby %Q{
+model = ContactModel.new
+assert_match /first_/, model.first_name
+assert_match /last_/, model.first_name
+}
   end
 
-  document :test_model_defaults, "Models", "Default Values" do
+
+  def define_trait
+    ruby %Q{
+module Watirmark::Model
+  trait :person do
+    first_name  {"first_\#{uuid}" }
+    last_name   {"last_\#{uuid}" }
+  end
+end
+}
+  end
+
+  def include_trait
+    ruby %Q{
+class ContactModel < Watirmark::Model.factory
+  keywords ContactPage.keywords
+  traits :person
+end
+
+class AdministratorModel < Watirmark::Model.factory
+  keywords ContactPage.keywords
+
+  traits :person
+
+  defaults do
+    security_group "Administrators"
+  end
+end
+
+class DonorModel < Watirmark::Model.factory
+  keywords ContactPage.keywords + DonationPage.keywords
+
+  traits :person, :donation
+end
+}
+  end
+
+
+  def define_trait_composition
+    ruby %Q{
+module Watirmark::Model
+  trait :credit_card do
+    credit_card_number      {4111111111111111}
+    credit_card_cvv         {111}
+    credit_card_expiry_date {Time.now.strftime "%m/%d/%Y"}
+  end
+
+  trait :donation do
+    amount    {10}
+    frequency {'monthly'}
+    traits :credit_card
+  end
+
+  trait :store_purchase do
+    item   {'neato item'}
+    traits :credit_card
+  end
+
+end
+}
+  end
+
+  document :test_model_basics, "Models" do
     %Q{
-    #{donor_model}
-    <p>Models can be defined with default values that you can access</p>
-    <pre class="ruby">
+    <p>Models provide us a way to organize and pass our test data to controllers.
 
-    model = SingleDonorModel.new
-    model.first_name.should == "John"
-    </pre>}
+    #{model}
+
+    <p>A model requires a list of keywords and then uses them to create getter and setter methods. By convention
+       we pass in the keywords from the view because we then only need to define our keywords in one place.
+
+    #{using_simple_model}
+
+    <p>This model can now be passed to a controller.new and the controller would populate the form using
+       these values. Where the real power of this starts becoming clear is when we take advantage
+       of the model features. Let's redefine our model, but this time define a set of default values.
+
+    #{model_with_defaults}
+
+    <p>Now we're getting somewhere. Every time I create a new instance of this model, I get new data*. This
+       means I can always create unique objects and not run into data collisions.
+
+    #{using_model_with_defaults}
+
+    <p valign="bottom">* The #uuid method generates a short, unique string
+    }
   end
 
-  document :test_model_value, "Models", "Getting Values"  do
-    %Q{
-    #{donor_model}
-    <p>Models can be defined with default values that you can access</p>
-    <pre class="ruby">
+  document :test_model_traits, "Models", "Traits" do
+     %Q{
+       <p>For a full-scale app you can imagine that there would be a large number of models and probably more
+       models than views. For example, our ContactPage has a ContactModel but you could imagine some
+       permutations like DonorModel, AdministratorModel, etc where these other models are still constituents
+       but might have a different set of defaults to fulfill their intended roles.
 
-      model = SingleDonorModel.new
-      model.first_name.should == "John"
-    </pre>}
+       <p>To handle this cleanly, we stole an idea from FactoryGirl and implemented the #traits feature.
+       #{define_trait}
+
+       <p>This sample defines a :person trait which we can then use in any model we create
+       #{include_trait}
+
+     }
   end
 
-  document :test_model_update_value, "Models", "Updating Default Values"  do
+  document :test_model_traits_containing_traits, "Models", "Composing Traits" do
     %Q{
-    #{donor_model}
-    <p>Models allow you to update there default values</p>
-    <pre class="ruby">
+       <p>Traits can also be composed, making them easier to reuse.
+       #{define_trait_composition}
 
-      model = SingleDonorModel.new
-      model.first_name "Alex"
-      model.first_name.should == "Alex"
-    </pre>}
-  end
-
-  document :test_model_update, "Models", "Updating Default Values"   do
-    %Q{
-    #{donor_model}
-    <p>Models allow you to update there default values</p>
-    <pre class="ruby">
-
-      model = SingleDonorModel.new
-      model.first_name "Alex"
-      model.first_name.should == "Alex"
-    </pre>}
-  end
-
-  document :test_model_methods, "Models" , "Using Model Methods"  do
-    %Q{
-    #{donor_model}
-    <p>You can define methods within a model that can be called</p>
-    <pre class="ruby">
-      model = SingleDonorModel.new
-      model.full_name.should == "Smith, Jon"
-    </pre>}
-  end
-
-  document :test_model_keywords, "Models", "Keywords"   do
-    %Q{
-    #{donor_model}
-    <p>Each models defines the keywords that can be used when populating page objects</p>
-    <pre class="ruby">
-
-      model = SingleDonorModel.new
-      model.keywords.should == SingleDonorModelView.keywords
-    </pre>}
-  end
-
-  document :test_controller_without_model, "Models", "Using with a controller"  do
-    %Q{
-    #{donor_model}
-    <p>Use models with your controllers to keep you tests clean and easy to read</p>
-    <pre class="ruby">
-
-      model = SingleDonorModel.new
-      DonationController.new(model).run :create
-    </pre>}
-  end
-
-  document :test_controller_with_model, "Models", "Using with a controller"  do
-    %Q{
-    #{donor_model}
-    <p>Use models with your controllers to keep you tests clean and easy to read</p>
-    <pre class="ruby">
-
-      model = SingleDonorModel.new
-      DonationController.new(model).run :create
-    </pre>}
+       <p>In this example we've defined a :credit_card trait and are able to reuse it to
+          generate other traits that have some form of payment.
+    }
   end
 
 end
--- src/about_models.rb
+++ src/about_models.rb
@@ -2,63 +2,70 @@ require_relative 'edgecase'
 
 class AboutModels < EdgeCase::Koan
 
-  def setup
-    show_page "page_object.html"
+  class ContactPage < Watirmark::Page
+    keyword(:first_name) { browser.text_field(:id, "first.name") }
+    keyword(:middle_name) { browser.text_field(:id, "middle.name") }
+    keyword(:last_name) { browser.text_field(:id, "last.name") }
   end
 
-  class DonationFormPage < Watirmark::Page
-    keyword(:firstname) { browser.text_field(:id, "first_name") }
-    keyword(:lastname) { browser.text_field(:id, "last_name") }
-  end
-
-  class DonationForm < Watirmark::WebPage::Controller
-    @view = DonationFormPage
-  end
-
-
-  class DonationModel < Watirmark::Model::Factory
-    keywords DonationFormPage.keywords
+  class ContactModel < Watirmark::Model::Factory
+    keywords ContactPage.keywords
 
     defaults do
-      firstname { "Robby" }
-      lastname { "Smith" }
-    end
-
-    def full_name
-      "#{firstname} #{lastname}"
+      first_name  {"first_#{uuid}" }
+      middle_name {"middle"}
+      last_name   {"last_#{uuid}" }
     end
   end
 
-  def test_model_defaults
-    assert_equal __(true), DonationModel.new.respond_to?(:firstname)
+  class Contact < Watirmark::WebPage::Controller
+    @view = ContactPage
   end
 
-  def test_model_value
-    assert_equal __("Robby"), DonationModel.new.firstname
+  def setup
+    show_page "page_object_addition.html"
   end
 
-  def test_model_update_value
-    model = DonationModel.new
-    model.firstname = "Joey"
-    assert_equal __("Joey"), model.firstname
+  def test_model_basics
+    assert_equal __("middle"), ContactModel.new.middle_name
+    assert_match __(/first_/), ContactModel.new.first_name
   end
 
-  def test_model_update
-    model = DonationModel.new.update(:middlename => "Middle")
-    assert_equal __(true), model.respond_to?(:middlename)
+  module ::Watirmark::Model
+    trait :person do
+      first_name  {"first_#{uuid}" }
+      last_name   {"last_#{uuid}" }
+    end
   end
 
-  def test_model_methods
-    assert_equal __("Robby Smith"), DonationModel.new.full_name
+
+  #trait :donor do
+  #  traits :person
+  #end
+
+  def test_model_traits
+    assert_equal __([:person]), Watirmark::Model::Traits.instance[:donor].traits
   end
 
-  def test_model_keywords
-    assert_equal __(DonationFormPage.keywords), DonationModel.new.keywords
+  def test_model_traits_containing_traits
+    assert_equal __([:donor]), DonorModelTrait.new.traits
   end
 
-  def test_controller_without_model
-    controller = DonationForm.new({:firstname => "Robby",
-                                   :lastname => "Smith"})
+  def test_model_without_trait_values
+    model = DonorModelWithOutTrait.new
+    assert_equal __("Robby"), model.firstname
+    assert_equal __("Smith"), model.lastname
+  end
+
+  def test_model_with_taits
+    model = DonorModelTrait.new
+    assert_equal __("Robby"), model.firstname
+    assert_equal __("Smith"), model.lastname
+  end
+
+  def test_model_without_traits_in_controller
+    model = DonorModelWithOutTrait.new
+    controller = Donationform.new(model)
     controller.run :populate_data
 
     page = DonationFormPage.new
@@ -66,9 +73,9 @@ class AboutModels < EdgeCase::Koan
     assert_equal __("Smith"), page.lastname.value
   end
 
-  def test_controller_with_model
-    model = DonationModel.new
-    controller = DonationForm.new(model)
+  def test_model_with_traits_in_controller
+    model = DonorModelTrait.new
+    controller = Donationform.new(model)
     controller.run :populate_data
 
     page = DonationFormPage.new
