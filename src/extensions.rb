9c1ce15ac2fc7b08a6a2ea57fc18541e32562aea
Justin Chang <jchang691@gmail.com>
2013-08-23 14:13:39 -0500

    Removes the check for post error. Added modified modal dialog for watirmark

--- src/extensions.rb
+++ src/extensions.rb
@@ -1,18 +1,5 @@
 require 'watir-webdriver'
 
-module Watir
-  class Button < HTMLElement
-
-    alias :old_button_click :click
-
-    def click
-      Watir::Wait.until { visible?}
-      old_button_click
-      Watirmark::Session.instance.check_for_post_errors
-    end
-  end
-end
-
 # stub out navigation errors
 module Watirmark
   class Page
@@ -20,6 +7,43 @@ module Watirmark
     end
 
     def edit(*args)
+    end
+  end
+end
+
+module Watirmark
+  module Dialogs
+    #Watir Frames do not have windows due. Watirmark's modal_exists assumes the Page
+    #is a browser
+    def modal_exists?
+      !!($koan_browser.windows.size > 1)
+    end
+
+    def with_modal_dialog
+      wait_for_modal_dialog
+      parent_window = ($koan_browser.windows.size) - 2
+      begin
+        $koan_browser.windows.last.use
+        $koan_browser.wait
+        yield
+      ensure
+        $koan_browser.windows[parent_window].use
+        Watirmark::Page.browser
+      end
+    end
+
+    def wait_for_modal_dialog
+      begin
+        Timeout::timeout(30) {
+          until modal_exists?
+            sleep 0.002
+          end
+          $koan_browser.wait
+          sleep 0.02
+        }
+      rescue Timeout::Error
+        raise Watirmark::TestError, 'Timed out while waiting for modal dialog to open'
+      end
     end
   end
 end
